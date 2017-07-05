080e93a8818555cc739b13c273461aa2b8bbc336
张兆卿 <zhangzhaoqing@zhangzhaoqingdeMacBook-Pro.local>
2017-07-05 10:13:47 +0800

    Initial Commit

---
+++ PCHDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PCHDemo
+//
+//  Created by 张兆卿 on 2017/7/5.
+//  Copyright © 2017年 xiaoSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
